23d526495ab6ab21e2233d69da4dcef01c57019f
Nicholas Homoelle <43354894+herringvoices@users.noreply.github.com>
2025-02-07 10:35:04 -0600

    Fixed import issue in ApplicationViews

--- apitemplate.sh
+++ apitemplate.sh
@@ -971,123 +971,57 @@ import { AuthorizedRoute } from "./auth/AuthorizedRoute";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import NavBar from "./NavBar";
-import CreateOrder from "./Orders/CreateOrder";
-import { Container } from "react-bootstrap";
-import { OrderList } from "./orders/OrderList";
-import { EmployeeForm } from "./Employees/EmployeeForm";
-import { useState } from "react";
-import { EmployeeList } from "./Employees/EmployeeList";
-import { EmployeeDetails } from "./Employees/EmployeeDetails";
-import OrderDetails from "./Orders/OrderDetails";
-import Welcome from "./Orders/Welcome";
 
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
-  const [edit, setEdit] = useState(false);
   return (
     <Routes>
+      {/* PARENT ROUTE */}
       <Route
         path="/"
+        // This element is your "layout" that wraps children in an <Outlet />
         element={
+          //Renders the NavBar above web content
           <>
             <NavBar
               loggedInUser={loggedInUser}
               setLoggedInUser={setLoggedInUser}
             />
-            
-              <Outlet />
-           
+
+            {/* All nested routes will appear here */}
+            <Outlet />
           </>
         }
       >
+        {/* CHILD ROUTES */}
+        {/* Home (index) route */}
         <Route
           index
           element={
             <AuthorizedRoute loggedInUser={loggedInUser}>
-              <Welcome loggedInUser={loggedInUser} />
+              <h1 className="text-center">Welcome</h1>
             </AuthorizedRoute>
           }
         />
-        <Route path="/orders">
-          <Route
-            index
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser}>
-                <OrderList />
-              </AuthorizedRoute>
-            }
-          />
-          <Route
-            path="create"
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser}>
-                <CreateOrder loggedInUser={loggedInUser} />
-              </AuthorizedRoute>
-            }
-          />
-          <Route
-            path=":id"
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser}>
-                <OrderDetails loggedInUser={loggedInUser} />
-              </AuthorizedRoute>
-            }
-          />
-          <Route
-            path=":id/edit"
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser}>
-                <CreateOrder edit={true} loggedInUser={loggedInUser} />
-              </AuthorizedRoute>
-            }
-          />
-        </Route>
-        <Route path="employees">
-          <Route
-            index
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
-                <EmployeeList />
-              </AuthorizedRoute>
-            }
-          />
-          <Route
-            path=":id"
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
-                <EmployeeDetails />
-              </AuthorizedRoute>
-            }
-          />
-          <Route
-            path=":id/edit"
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
-                <EmployeeForm edit={true} setEdit={setEdit} />
-              </AuthorizedRoute>
-            }
-          />
-          <Route
-            path="create"
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
-                <EmployeeForm edit={false} setEdit={setEdit} />
-              </AuthorizedRoute>
-            }
-          />
-        </Route>
+
+        {/* /login route */}
         <Route
           path="login"
           element={<Login setLoggedInUser={setLoggedInUser} />}
         />
+
+        {/* /register route */}
         <Route
           path="register"
           element={<Register setLoggedInUser={setLoggedInUser} />}
         />
       </Route>
+
+      {/* CATCH-ALL ROUTE */}
       <Route path="*" element={<p>Whoops, nothing here...</p>} />
     </Routes>
   );
 }
+
 
 
 EOF
